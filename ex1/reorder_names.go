a16e51d4ce126fb5c64a251195dff1ccb1076e0c
Nguyen Dat <official.nguyendat@gmail.com>
2023-07-20 14:01:40 +0700

    Fix middleName logic

--- ex1/reorder_names.go
+++ ex1/reorder_names.go
@@ -3,62 +3,54 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 )
 
-func printResult(firstnName, lastName, middleName string) {
-	if middleName == "" {
-		fmt.Print(firstnName, " ", lastName)
-	} else {
-		fmt.Print(firstnName, " ", middleName, " ", lastName)
-	}
-}
-
-func validContext(context []string, formatCode string) bool {
-	if len(context) > 4 {
-		fmt.Print("Invalid Input")
-		return true
-	} else if formatCode != "VN" && formatCode != "US" {
+func isValidContext(context []string, formatCode string) bool {
+	if formatCode != "VN" && formatCode != "US" {
 		fmt.Print("Invaild Country Code")
 		return true
 	} else if context[1] == formatCode {
-		fmt.Print("Invalid Name ")
+		fmt.Print("Invalid Input ")
 		return true
 	}
 	return false
 }
 
-func orderVietNam(context []string) (string, string, string) {
-	firstname := context[1]
-	lastname := context[0]
-	var middleName string
-	if context[2] != "VN" {
-		firstname = context[1]
-		middleName = context[2]
-		lastname = context[0]
-	}
-	return firstname, lastname, middleName
-}
+func orderNameFollowCountryCode(context []string, countryCode string) {
+	firstName := context[0]
+	lastName := context[1]
+	switch countryCode {
+	case "VN":
+		{
+			if context[2] != countryCode {
+				firstName = context[1]
+				lastName = context[0]
+				middleName := strings.Join(context[2:len(context)-1], " ")
 
-func orderUS(context []string) (string, string, string) {
-	firstname := context[0]
-	lastname := context[1]
-	var middleName string
-	if context[2] != "US" {
-		firstname = context[0]
-		middleName = context[1]
-		lastname = context[2]
+				fmt.Printf("%s %s %s", lastName, middleName, firstName)
+				break
+			}
+			fmt.Printf("%s %s", lastName, firstName)
+		}
+	case "US":
+		{
+			if context[2] != countryCode {
+				middleName := strings.Join(context[2:len(context)-1], " ")
+				fmt.Printf("%s %s %s", firstName, middleName, lastName)
+				break
+			}
+			fmt.Printf("%s %s", firstName, lastName)
+		}
 	}
-	return firstname, lastname, middleName
 }
 
 func main() {
 	context := os.Args[1:]
 	formatCode := context[len(context)-1]
-	if validContext(context, formatCode) {
+	if isValidContext(context, formatCode) {
 		os.Exit(0)
-	} else if formatCode == "VN" {
-		printResult(orderVietNam(context))
 	} else {
-		printResult(orderUS(context))
+		orderNameFollowCountryCode(context, formatCode)
 	}
 }
